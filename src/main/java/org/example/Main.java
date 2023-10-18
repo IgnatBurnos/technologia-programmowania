98a1ee254da56ffbdd10cf008fa066d71672160d
IgnatBurnos <ignat.burnos@gmail.com>
2023-10-18 15:38:25 +0200

    goodgrade

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         int boardSize = 44;
         int numPlayers = 2;
-        String[] playerNames = { "Player b", "Player a" };
+        String[] playerNames = { "Player a", "Player b" };
 
         Game game = new Game(boardSize, playerNames);
         game.play();
